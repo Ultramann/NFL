821750b3c53a287a7254da98893fb5b5df6bedaa
Ultramann <cary.goltermann@gmail.com>
2015-10-13 09:33:31 -0600

    Changed to Class for keeping track of nfldb tables

--- eda.py
+++ eda.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import nfldb_tables
 import matplotlib.pyplot as plt
+from nfldb_tables import NFL_Frames
 
 
 def check_player_table(player):
@@ -15,40 +15,23 @@ def check_player_table(player):
     print 'All of the position UNKs have Unknown status: {}'.format(truth)
 
 
-def fandual_points_offense(in_df):
+def make_player_game_points(nfl_frames, year=None):
     '''
-    Input:  DataFrame matching play_player format of nfldb
-    Output: Series containing FanDual points for each row in input DataFrame
-
-    Based on https://www.fanduel.com/rules
-    Currently only calculates points for offensive players
-    '''
-    fandual_points = \
-                        0.1*in_df.rushing_yds + 6*in_df.rushing_tds + \
-                        0.04*in_df.passing_yds + 4*in_df.passing_tds - 1*in_df.passing_int + \
-                        0.1*in_df.receiving_yds + 6*in_df.receiving_tds + 0.5*in_df.receiving_rec + \
-                        6*in_df.kickret_tds + 6*in_df.puntret_tds - 2*in_df.fumbles_lost + \
-                        6*in_df.fumbles_rec_tds + 2*in_df.receiving_twoptm + \
-                        3*in_df.kicking_fgm + 1*(in_df.kicking_fgm_yds>=40) + \
-                        1*(in_df.kicking_fgm_yds>=50) + 1*in_df.kicking_xpmade
-
-    return fandual_points
-
-
-def make_player_game_points(g, pp_df, p_df, year=None):
-    '''
-    Input:  DataFrame of games, DataFame of play_player, DataFame of players, Int of year
+    Input:  NFL_Frames, Int of year
     Output: DataFrame with aggregated fanduel points for players, 
             optionally, who played in a given year
     '''
     year_query = 'season_year == @year' if year else 'season_year > 0' 
-    pp = pp_df.merge(right=g[['gsis_id', 'season_year']], how='left', on='gsis_id').query(year_query)
-    game_points = pp.groupby(['player_id', 'gsis_id'], as_index=False).offensive_points.sum()
-    player_game_points = game_points.merge(
-                                           right=p_df[['player_id', 'full_name', 'position']],
+    ppg = nfl_frames.play_player.merge(right=nfl_frames.game[['gsis_id', 'season_year']], 
+                                       how='left', 
+                                       on='gsis_id')
+    ppgy = ppg.query(year_query)
+    game_points = ppgy.groupby(['player_id', 'gsis_id'], as_index=False).fanduel_points.sum()
+    player_columns = ['player_id', 'full_name', 'position']
+    player_game_points = game_points.merge(right=nfl_frames.player[player_columns],
                                            how='left', 
-                                           on='player_id',
-                                          )
+                                           on='player_id')
+                                        
     return player_game_points
 
 
@@ -57,15 +40,18 @@ def graph_offense_stats_summary(player_game_points, verbose=False, bins=50):
     Input:  DataFrame of player point per game
     Output: None
 
-    Prints a description of fanduel point production by position for offensive players
+    Graphs fanduel point production by position for offensive players, optionally prints description 
     '''
+    # Only want to grab the offensive point producing positions
     offensive_positions = ['QB', 'RB', 'WR', 'TE', 'K']
     offensive_query = 'position in {}'.format(offensive_positions)
-    offensive_gb = player_game_points.query(offensive_query).groupby('position').offensive_points
+    offensive_gb = player_game_points.query(offensive_query).groupby('position').fanduel_points
+
     if verbose:
         print('Summary statistics of FanDual points by position')
         print(offensive_gb.describe())
 
+    # Cycle through the group by object and graph the data for each position
     plt.figure(figsize=(15, 8))
     for i, pos in enumerate(offensive_gb, 1):
         plt.subplot(2, 3, i)
@@ -74,23 +60,28 @@ def graph_offense_stats_summary(player_game_points, verbose=False, bins=50):
     plt.show()
 
 
-def get_year_week_frame(year, week, game, play_player, player, season_type='Regular'):
+def get_year_week_frame(year, week, nfl_frames, season_type='Regular'):
     '''
-    Input:  DataFrame of games, DataFrame for play_player, DataFrame of players, Int, Int, Str
+    Input:  Year as Int, Week as Int, NFL_Frame, Str
     Output: DataFrame of stats for a given week
 
     Returns DataFrame with all players aggregated stats from a specifc year and week
     '''
     # Make DataFrame games from the specified year, season type and week
-    week_df = game.query('season_type == @season_type & season_year == @year & week == @week')
+    query_string = 'season_type == @season_type & season_year == @year & week == @week'
+    week_df = nfl_frames.game.query(query_string)
 
     # Get all of the play information for all of those games
-    super_week_df = pd.DataFrame(week_df['gsis_id']).merge(play_player, how='left', on='gsis_id')
+    super_week_df = pd.DataFrame(week_df['gsis_id']).merge(nfl_frames.play_player, 
+                                                           how='left', 
+                                                           on='gsis_id')
     agg_week_df = super_week_df.groupby(['gsis_id', 'player_id'], as_index=False).sum()
 
     # Add in the names of the players from the player frame
     player_columns = ['player_id', 'full_name', 'position', 'team']
-    agg_week_names_df = agg_week_df.merge(player[player_columns], how='left', on='player_id')
+    agg_week_names_df = agg_week_df.merge(nfl_frames.player[player_columns], 
+                                          how='left', 
+                                          on='player_id')
 
     # Add in the home and away teams
     team_columns = ['gsis_id', 'home_team', 'away_team']
@@ -103,10 +94,9 @@ def get_year_week_frame(year, week, game, play_player, player, season_type='Regu
     return agg_week_names_df.set_index('full_name')
 
 
-def get_position_year_week_frame(position, year, week, 
-                                 game, play_player, player, season_type='Regular'):
+def get_position_year_week_frame(position, year, week, nfl_frames, season_type='Regular'):
     '''
-    Input:  Str, DataFrame of games, DataFrame for play_player, DataFrame of players, Int, Int, Str
+    Input:  Str, Year as Int, Week as Int, NFL_Frames, Str
     Output: DataFrame of stats for a players in given position during a given week
     '''
     # Dictionary of stats by type
@@ -123,20 +113,19 @@ def get_position_year_week_frame(position, year, week,
                      'WR': offense_types['recp'],
                      'TE': offense_types['recp']}
                          
-    df = get_year_week_frame(year, week, game, play_player, player, season_type)
+    df = get_year_week_frame(year, week, nfl_frames, season_type)
 
     position_columns = ['position', 'team', 'opponent'] + \
                        position_dict[position] + \
-                       ['offensive_points']
+                       ['fanduel_points']
 
     return df.query('position == @position')[position_columns]
 
 
 if __name__ == '__main__':
-    game, pp, player = nfldb_tables.get(['game', 'play_player', 'player'])
-    check_player_table(player)
-    pp['offensive_points'] = fandual_points_offense(pp)
-    player_game_points = make_player_game_points(game, pp, player)
-    player_game_points_2014 = make_player_game_points(game, pp, player, year=2014)
+    nfl_frames = NFL_Frames() 
+    check_player_table(nfl_frames.player)
+    player_game_points = make_player_game_points(nfl_frames)
+    player_game_points_2014 = make_player_game_points(nfl_frames, year=2014)
     graph_offense_stats_summary(player_game_points)
 
--- nfldb_tables.py
+++ nfldb_tables.py
@@ -18,12 +18,49 @@ class nfldb_conn(object):
         '''
         return pd.read_sql('SELECT * FROM {}'.format(table_name), self.conn)
 
-def get(tables):
+
+class NFL_Frames(object):
     '''
-    Input:  List of table names to get from nfldb
-    Output  List of Dataframes for corresponding table names
+    Class to hold the most frequently used nfldb tables (game, play_player, player) 
+    as frames all bundled together
     '''
-    nfldb = nfldb_conn()
-    frames = [nfldb.get_table(table) for table in tables]
-    nfldb.close()
-    return frames
+    def __init__(self, tables=['game', 'play_player', 'player']):
+        self.get(tables) 
+        self.make_fanduel_points()
+
+    def get(self, tables):
+        '''
+        Input:  List of table names to get from nfldb
+
+        Makes attributes corresponding to the table names
+        '''
+        nfldb = nfldb_conn()
+        frames = [nfldb.get_table(table) for table in tables]
+        self.game, self.play_player, self.player = frames
+        nfldb.close()
+
+    def make_fanduel_points(self):
+        '''
+        Calculates the fanduel point production based on https://www.fanduel.com/rules
+        Currently only calculates points for offensive players
+        '''
+        fanduel_points = ( 0.1 * self.play_player.rushing_yds 
+                         + 6 * self.play_player.rushing_tds 
+                         + 0.04 * self.play_player.passing_yds 
+                         + 4*self.play_player.passing_tds 
+                         - 1*self.play_player.passing_int 
+                         + 0.1*self.play_player.receiving_yds 
+                         + 6*self.play_player.receiving_tds 
+                         + 0.5*self.play_player.receiving_rec 
+                         + 6*self.play_player.kickret_tds 
+                         + 6*self.play_player.puntret_tds 
+                         - 2*self.play_player.fumbles_lost 
+                         + 6*self.play_player.fumbles_rec_tds 
+                         + 2*self.play_player.receiving_twoptm 
+                         + 3*self.play_player.kicking_fgm 
+                         + 1*(self.play_player.kicking_fgm_yds>=40) 
+                         + 1*(self.play_player.kicking_fgm_yds>=50) 
+                         + 1*self.play_player.kicking_xpmade)
+
+        self.play_player['fanduel_points'] = fanduel_points
+
