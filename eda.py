adccd4a1c3172a20962aebf9bac6a851e4249f3f
Ultramann <cary.goltermann@gmail.com>
2015-10-14 09:22:30 -0600

    More changes to the classes functionality. I hope it's better now

--- eda.py
+++ eda.py
@@ -59,69 +59,6 @@ def graph_offense_stats_summary(player_game_points, verbose=False, bins=50):
         plt.title(pos[0])
     plt.show()
 
-
-def get_year_week_frame(year, week, nfl_frames, season_type='Regular'):
-    '''
-    Input:  Year as Int, Week as Int, NFL_Frame, Str
-    Output: DataFrame of stats for a given week
-
-    Returns DataFrame with all players aggregated stats from a specifc year and week
-    '''
-    # Make DataFrame games from the specified year, season type and week
-    query_string = 'season_type == @season_type & season_year == @year & week == @week'
-    week_df = nfl_frames.game.query(query_string)
-
-    # Get all of the play information for all of those games
-    super_week_df = pd.DataFrame(week_df['gsis_id']).merge(nfl_frames.play_player, 
-                                                           how='left', 
-                                                           on='gsis_id')
-    agg_week_df = super_week_df.groupby(['gsis_id', 'player_id'], as_index=False).sum()
-
-    # Add in the names of the players from the player frame
-    player_columns = ['player_id', 'full_name', 'position', 'team']
-    agg_week_names_df = agg_week_df.merge(nfl_frames.player[player_columns], 
-                                          how='left', 
-                                          on='player_id')
-
-    # Add in the home and away teams
-    team_columns = ['gsis_id', 'home_team', 'away_team']
-    agg_week_names_df = agg_week_names_df.merge(week_df[team_columns], how='left', on='gsis_id')
-
-    # Compute opponent
-    opp = lambda x: x.away_team if x.away_team != x.team else x.home_team
-    agg_week_names_df['opponent'] = agg_week_names_df.apply(opp, axis=1)
-
-    return agg_week_names_df.set_index('full_name')
-
-
-def get_position_year_week_frame(position, year, week, nfl_frames, season_type='Regular'):
-    '''
-    Input:  Str, Year as Int, Week as Int, NFL_Frames, Str
-    Output: DataFrame of stats for a players in given position during a given week
-    '''
-    # Dictionary of stats by type
-    offense_types = {'pass': ['passing_att', 'passing_cmp', 'passing_cmp_air_yds', 'passing_incmp',
-                              'passing_incmp_air_yds', 'passing_int', 'passing_yds', 'passing_tds'],
-                     'rush': ['rushing_att', 'rushing_loss', 'rushing_loss_yds', 'rushing_yds',
-                              'rushing_tds'],
-                     'recp': ['receiving_rec', 'receiving_tar', 'receiving_yac_yds', 'receiving_yds',
-                              'receiving_tds']}
-
-    # Dictionary of stats by position
-    position_dict = {'QB': offense_types['pass'] + offense_types['rush'],
-                     'RB': offense_types['rush'] + offense_types['recp'],
-                     'WR': offense_types['recp'],
-                     'TE': offense_types['recp']}
-                         
-    df = get_year_week_frame(year, week, nfl_frames, season_type)
-
-    position_columns = ['position', 'team', 'opponent'] + \
-                       position_dict[position] + \
-                       ['fanduel_points']
-
-    return df.query('position == @position')[position_columns]
-
-
 if __name__ == '__main__':
     nfl_frames = NFL_Frames() 
     check_player_table(nfl_frames.player)
