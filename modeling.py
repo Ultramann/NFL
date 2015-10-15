6ec7ec7db64322ae2060f664680b7d7c94fa2241
Ultramann <cary.goltermann@gmail.com>
2015-10-15 08:47:17 -0600

    Changed getting fanduel points in year till week to non-position wise

--- modeling.py
+++ modeling.py
@@ -1,7 +1,8 @@
+import nimfa
 import pandas as pd
 from nfldb_tables import NFL_Frames
 
-def get_position_year_tillweek_fanduel_points(nfl_frames, position, year, max_week):
+def get_fanduel_points_year_till_week(nfl_frames, year, till_week):
     '''
     Input:  NFL_Frames, position as Str, Year as Int, Week as Int
     Output: DataFrame of players of given position, their player_id, and the corresponding fanduel
@@ -13,13 +14,22 @@ def get_position_year_tillweek_fanduel_points(nfl_frames, position, year, max_we
         given year until max_week
         '''
         columns = ['player_id', 'opponent', 'fanduel_points']
-        return nfl_frames.get_position_year_week_frame(position, year, week)[columns]
+        return nfl_frames.get_year_week_frame(year, week)[columns]
 
-    weeks = range(1, max_week + 1)
+    weeks = range(1, till_week + 1)
     position_opp_fdp_year = pd.concat([get_fanduel_opp(week) for week in weeks], axis=0)
     return position_opp_fdp_year
 
+
+def decompose_things(year_position_frame):
+    '''
+    Input:  DataFrame with columns player_id, opponent, fanduel_points
+    Output: Some NMF decomposed stuff
+    '''
+    pass
+
+
 if __name__ == '__main__':
     nfl_frames = NFL_Frames() 
-    wr_2015_5 = get_position_year_tillweek_fanduel_points(nfl_frames, 'WR', 2015, 5) 
+    wr_2015_5 = get_fanduel_points_year_till_week(nfl_frames, 2015, 5) 
     
