5488f45d84cbbc7be12d9a97d86183d81affd882
Ultramann <cary.goltermann@gmail.com>
2015-10-16 11:19:01 -0600

    Making progress with NMF decomposition class

--- modeling.py
+++ modeling.py
@@ -3,67 +3,89 @@ import pandas as pd
 from nfldb_tables import NFL_Frames
 from scipy.sparse import csr_matrix as csrm
 
-def get_fanduel_points_year_till_week(nfl_frames, year, till_week):
-    '''
-    Input:  NFL_Frames, position as Str, Year as Int, Week as Int
-    Output: DataFrame of players of given position, their player_id, and the corresponding fanduel
-            points - opponent those points were scored against pairs
-    '''
-    def get_fanduel_opp(week):
+
+class NMF_Skillz(object):
+    def __init__(self, year, till_week):
+        self.year = year
+        self.till_week = till_week
+        self.nfl_frames = NFL_Frames()
+        self.make_year_till_week()
+
+    def make_year_till_week(self):
         '''
-        Helper function to grab the player_id, opponent, and fanduel points for a position in the
-        given year until max_week
+        Output: DataFrame of players of given position, their player_id, and the corresponding 
+                fanduel points and opponent those points were scored against
         '''
-        columns = ['player_id', 'position', 'opponent', 'fanduel_points']
-        return nfl_frames.get_year_week_frame(year, week)[columns]
+        def get_fanduel_opp(week):
+            '''
+            Helper function to grab the player_id, opponent, and fanduel points for a position 
+            in the given year until max_week
+            '''
+            columns = ['player_id', 'position', 'opponent', 'fanduel_points']
+            return self.nfl_frames.get_year_week_frame(self.year, week)[columns]
 
-    weeks = range(1, till_week + 1)
-    position_opp_fdp_year = pd.concat([get_fanduel_opp(week) for week in weeks], axis=0)
-    return position_opp_fdp_year
+        weeks = range(1, self.till_week + 1)
+        self.year_till_week = pd.concat([get_fanduel_opp(week) for week in weeks], axis=0)
 
+    def get_position(self, position): 
+        '''
+        Input:  Str - position to get latent skills for
+        Output: DataFrame - player names and latent skills for position
+        '''
+        position_sparse, player_ids, opponents = self.make_sparse_position_pivot_table(position)
+        position_skill = self.decompose_position(position_sparse, player_ids, opponents)
 
-def make_sparse_position_pivot_table(position, year_till_week_df):
-    position_subset_df = year_till_week_df.query('position == @position')
+        # Prepare to merge back with player names for human readability
+        names_columns = ['player_id', 'full_name']
+        position_names = self.nfl_frames.player.query('position == @position')[names_columns]
 
-    # Get unique player ids and opponent lists 
-    player_ids = position_subset_df.player_id.unique().tolist()
-    opponents = position_subset_df.opponent.unique().tolist()
-    matrix_shape = position_subset_df.player_id.nunique(), position_subset_df.opponent.nunique()
+        skill_names =  position_names.merge(right=position_skill,
+                                            how='inner',
+                                            right_index=True,
+                                            left_on='player_id') 
+        return skill_names.sort(columns=['skill'], ascending=False)
 
-    # Make some categories
-    player_cats = position_subset_df.player_id.astype('category', categories=player_ids).cat.codes
-    opp_cats = position_subset_df.opponent.astype('category', categories=opponents).cat.codes
+    def make_sparse_position_pivot_table(self, position):
+        '''
+        Input:  Position for which to get the NMF decomposition latent features
+        Output: Sparse matrix (player count in position x opponents) of fanduel points scored,
+                list of player_ids for the sparse matrix, list of opponents for the sparse matrix
+        '''
+        position_df = self.year_till_week.query('position == @position')
 
-    # Get rid of the negative point production for now
-    fanduel_points = position_subset_df.fanduel_points.clip(lower=0).values
+        # Get unique player ids and opponent lists 
+        player_ids = position_df.player_id.unique().tolist()
+        opponents = position_df.opponent.unique().tolist()
+        matrix_shape = position_df.player_id.nunique(), position_df.opponent.nunique()
 
-    sparse_position_matrix = csrm((fanduel_points, (player_cats, opp_cats)), shape=matrix_shape)
+        # Make some categories
+        player_cats = position_df.player_id.astype('category', categories=player_ids).cat.codes
+        opp_cats = position_df.opponent.astype('category', categories=opponents).cat.codes
 
-    return sparse_position_matrix, player_ids, opponents
+        # Get rid of the negative point production for now
+        fanduel_points = position_df.fanduel_points.clip(lower=0).values
 
+        sparse_position_matrix = csrm((fanduel_points, (player_cats, opp_cats)), shape=matrix_shape)
 
-def get_position_till_week(position, till_week, year_till_week_df):
-    pass
+        return sparse_position_matrix, player_ids, opponents
 
-
-def decompose_things(position_sparse, player_ids, opponents):
-    '''
-    Input:  Sparse Matrix of fanduel points (rows-player_ids, columns-opponents), 
-            list of player_ids, list of opponents
-    Output: 
-    '''
-    nmf = nimfa.Snmf(wr_2015_sparse, max_iter=100000, rank=1, update='euclidean', objective='fro')
-    wr_2015_nmf = nmf()
-    wr_skill = pd.DataFrame(wr_2015_nmf.basis().toarray(), index=player_ids, columns='skill')
-    return wr_skill
-
+    def decompose_position(self, position_sparse, player_ids, opponents):
+        '''
+        Input:  Sparse Matrix - fanduel points (rows-player_ids, columns-opponents), 
+                list of player_ids positionally associated with each row 
+        Output: DataFrame - index of player_ids, skill from single latent feature NMF decomposition 
+        '''
+        nmf = nimfa.Snmf(position_sparse, 
+                         max_iter=10000, 
+                         rank=1, 
+                         update='euclidean', 
+                         objective='fro')
+        position_nmf = nmf()
+        position_skill = pd.DataFrame(position_nmf.basis().toarray(), 
+                                      index=player_ids, columns=['skill'])
+        return position_skill
 
 if __name__ == '__main__':
-    nfl_frames = NFL_Frames() 
-    twenty_fifteen_5 = get_fanduel_points_year_till_week(nfl_frames, 2015, 5) 
-    wr_2015_sparse, player_ids, opponents = make_sparse_position_pivot_table('WR', twenty_fifteen_5) 
-    wr_2015_skill = nfl_frames.player.query('position == "WR"'). \
-                    merge(right=decompose_things(wr_2015_sparse, player_ids, opponents), 
-                          how='inner',
-                          right_index=True,
-                          left_on='player_id') 
+    skillz = NMF_Skillz(2015, 5)
+    wr_2015_5 = skillz.get_position('WR')
+    te_2015_5 = skillz.get_position('TE')
