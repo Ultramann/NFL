f179eb184cbbb47499c1e4495a6c10119ba38a1f
Ultramann <cary.goltermann@gmail.com>
2015-10-22 10:39:44 -0600

    Looks like year_week_position_rmse is functioning

--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 from nfldb_tables import NFLFrames
 from modeling import PositionNMFFactory
 
-def validate_year_week_position(year, week, predictions, nfl_frames):
+def year_week_position_rmse(year, week, position, predictions, nfl_frames):
     '''
     Input:  Int - year, Int - week to validate, Str - position predictions are for,
             DataFrame - predictions against, NFLFrames
@@ -9,22 +9,21 @@ def validate_year_week_position(year, week, predictions, nfl_frames):
     '''
     week_position_df = nfl_frames.get_year_week_frame(year, week).query('position == @position')
     week_postion_predictions_df = week_position_df.merge(right=predictions,
-                                                         how='left',
+                                                         how='inner',
                                                          on='player_id')
-    week_postion_predictions_df.eval('difference = fanduel_points - prediction')
+    week_postion_predictions_df.eval('error = fanduel_points - prediction')
     
-    # Do some stuff to difference column to get RMSE
-    return week_postion_predictions_df.rmse
+    rmse =  ((week_postion_predictions_df.error.values ** 2) ** 0.5).mean()
+
+    return rmse
 
 if __name__ == '__main__':
     nfl_frames = NFLFrames()
+    position = 'WR'
     year = 2015
     week = 5
     skill_factory = PositionNMFFactory(year, week, nfl_frames)
-    wr_2015_5 = skill_factory.get_position_model('WR')
+    wr_2015_5 = skill_factory.get_position_model(position)
     week_6_opps = nfl_frames.get_year_weeks_opponents(year=year, week=week+1)
     week_6_predictions = wr_2015_5.predict(week_6_opps)
-    '''
-    Next step:
-    some_number = validate_year_week_position(year, week, 'WR', nfl_frames, week_6_predictions)
-    '''
+    wr_2015_6_rmse = year_week_position_rmse(year, week, position, week_6_predictions, nfl_frames)
